4b8feb07a098d7836cbbce25a809ab6e9eb0310d
Gautam Kachru <gkachru@users.noreply.github.com>
2018-05-05 00:32:23 +0530

    Updated for Django v1.11

--- lazy_admin/views.py
+++ lazy_admin/views.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.apps import apps
 from django.contrib import admin
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseNotFound, HttpResponse
@@ -9,48 +9,49 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 @staff_member_required
 def lazy_column_view(request):
-	'''
-	Handles all lazy column ajax requests. Decorator allows only
-	admin users access to this view.
-	'''
-	if request.method == 'POST':
-		# Extract data from POST
-		try:
-			app = request.POST['app']
-			model_name = request.POST['model']
-			method = request.POST['method']
-			objid = request.POST['objid']
-		except KeyError:
-			return HttpResponseNotFound()
+    '''
+    Handles all lazy column ajax requests. Decorator allows only
+    admin users access to this view.
+    '''
+    if request.method == 'POST':
+        # Extract data from POST
+        try:
+            app = request.POST['app']
+            model_name = request.POST['model']
+            method = request.POST['method']
+            objid = request.POST['objid']
+        except KeyError:
+            return HttpResponseNotFound()
 
-		# Make sure Object ID is a number
-		try:
-			objid = long(objid)
-		except ValueError:
-			return HttpResponseNotFound()
+        # Make sure Object ID is a number
+        try:
+            objid = long(objid)
+        except ValueError:
+            return HttpResponseNotFound()
 
-		# Get the model class
-		model = models.get_model(app, model_name)
-		if model is None:
-			return HttpResponseNotFound()
+        # Get the model class
+        django_app = apps.get_app_config(app)
+        model = django_app.get_model(model_name, require_ready=True)
+        if model is None:
+            return HttpResponseNotFound()
 
-		# Now get the actual object
-		obj = get_object_or_404(model, pk=objid)
+        # Now get the actual object
+        obj = get_object_or_404(model, pk=objid)
 
-		# Validate if the user has change permission for this object
-		model_admin = None
-		try:
-			model_admin = admin.site._registry[model]
-			if not model_admin.has_change_permission(request, obj):
-				return HttpResponseNotFound()
-		except KeyError:
-			return HttpResponseNotFound()
+        # Validate if the user has change permission for this object
+        model_admin = None
+        try:
+            model_admin = admin.site._registry[model]
+            if not model_admin.has_change_permission(request, obj):
+                return HttpResponseNotFound()
+        except KeyError:
+            return HttpResponseNotFound()
 
-		# Get the admin method to be called
-		try:
-			func = getattr(model_admin, method)
-		except AttributeError:
-			return HttpResponseNotFound()
+        # Get the admin method to be called
+        try:
+            func = getattr(model_admin, method)
+        except AttributeError:
+            return HttpResponseNotFound()
 
-		# Finally! Call the method and send the output
+        # Finally! Call the method and send the output
         return HttpResponse(func(obj, delay=False))
