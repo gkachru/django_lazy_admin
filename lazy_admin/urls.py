d7c284426b51bd58188e4a5a3ae3bf913d29de14
Gautam Kachru <gkachru@users.noreply.github.com>
2018-05-04 22:40:36 +0530

    Updated for django 1.11

--- lazy_admin/urls.py
+++ lazy_admin/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, url
-from views import lazy_column_view
+from django.conf.urls import url
+from . import views
 
-urlpatterns = patterns('',
-	url(r'column/$', lazy_column_view, name='lazy-column-view')
-)
+urlpatterns = [
+	url(r'column/$', views.lazy_column_view, name='lazy-column-view')
+]
